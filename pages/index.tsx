2317e90eb415af1f1d34895be24cc64193af64bb
Yuncong Zhang <zhang.consulting.corp@gmail.com>
2022-02-03 19:30:45 +0000

    add flag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import requestIp from 'request-ip';
@@ -20,34 +21,48 @@ const Home: NextPage<{data: {ip: string, geo: Lookup, ua: string}}> = ({data}) =
           Welcome to <a href="http://ifconfig.zhang-consulting.com">ifconfig</a>
         </h1>
 
-        <div className='container'>
-          <div>
-            <span className='font-bold'>IP</span>:&nbsp;
-            <span>{data.ip}</span>
+        <h2>General Information</h2>
+        <div>
+          {
+            data.geo ?
+            (
+              <div className='d-flex align-items-center'>
+                <div className='font-bold'>Location</div>
+                <div>
+                  :&nbsp;{data.geo.city}, {data.geo.region}, {data.geo.country}&nbsp;
+                </div>
+                <div>
+                  <Image alt={`${data.geo.country} flag`} src={`https://flagcdn.com/96x72/${data.geo.country.toLowerCase()}.png`} width={32} height={32}/>
+                </div>
+              </div>
+            ) :
+            ''
+          }
+          <div className='d-flex'>
+            <div className='font-bold'>IP</div>
+            <div>:&nbsp;{data.ip}</div>
           </div>
-          <div>
-            <span className='font-bold'>Location</span>:&nbsp;
-            <span>{data.geo ? `${data.geo.city} ${data.geo.region} ${data.geo.country}` : ''}</span>
+          <div className='d-flex'>
+            <div className='font-bold'>User Agent</div>
+            <div>:&nbsp;{data.ua}</div>
           </div>
-          <span className='font-bold'>User Agent</span>:&nbsp;
-          <span>{data.ua}</span>
         </div>
 
         <h2>APIs</h2>
         <div>
           <div>
             <Link href="/api/ip">
-              <a>IP</a>
+              <a>IP -&gt;</a>
             </Link>
           </div>
           <div>
             <Link href="/api/geo">
-              <a>Geo</a>
+              <a>Geo -&gt;</a>
             </Link>
           </div>
           <div>
             <Link href="/api/ua">
-              <a>User Agent</a>
+              <a>User Agent -&gt;</a>
             </Link>
           </div>
         </div>
